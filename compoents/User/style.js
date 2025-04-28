8e25a3e64019332874f09c8c65ed29b348a3fbd4
muhammad adnan <adnanchaudhary.m144@gmail.com>
2025-04-28 12:36:00 +0500

    Initial commit for AwesomeProject

---
+++ compoents/User/style.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+  storyContainer: {
+    marginRight: 20,
+  },
+  firstName: {
+    fontWeight:500,
+    fontFamily:'Inter',
+    fontSize: 14,
+    color: '#022150',
+    marginTop: 8,
+    textAlign: 'center',
+  },
+});
+
+export default style;
---
+++ compoents/title/style.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+    title: {
+      color: '#022150',
+      fontFamily: 'Inter',
+      fontSize: 24,
+      fontWeight:600
+    },
+  });
+
+  export default style;
